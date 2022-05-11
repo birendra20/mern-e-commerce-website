232df3ce12b4c335365911010e0bf4d54ce1f6a7
birendra20 <birendragupta20@gmail.com>
2022-05-11 20:08:29 +0530

    .

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,11 @@ const app = require("./app");
 const dotenv = require("dotenv");
 const connectDatabase = require("./config/database");
 
+//config
 dotenv.config({ path: "backend/config/config.env" });
+
+// Connecting to database
+connectDatabase();
 
 const server = app.listen(8080, () => {
   console.log(`Server is working on http://localhost:${process.env.PORT}`);
